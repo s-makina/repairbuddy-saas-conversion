8a7ce7243cc37cbef4acb77807db66a210aec4a4
Salvation Makina <salvationemakina3@gmail.com>
2026-03-22 21:06:35 +0200

    nav

--- backend/resources/views/tenant/partials/tenant-nav.blade.php
+++ backend/resources/views/tenant/partials/tenant-nav.blade.php
@@ -23,14 +23,14 @@
 <nav class="rb-navbar">
     <div class="nav-inner">
         <a href="{{ $tenantHomeUrl }}" class="nav-brand">
+            <div>
             @if($tenant && $tenant->logo_url)
                 <img src="{{ $tenant->logo_url }}" alt="{{ $tenant->name }}" class="shop-logo-img">
             @else
                 <img src="{{ asset('brand/logo.png') }}" alt="RepairBuddy" class="shop-logo-img">
             @endif
-            <div>
-                <div class="shop-name">{{ $tenant->name ?? 'RepairBuddy' }}</div>
-                <div class="shop-sub">{{ $tenantSlug }}.{{ config('tenancy.base_domain') }}</div>
+                <!-- <div class="shop-name">{{ $tenant->name ?? 'RepairBuddy' }}</div> -->
+                <!-- <div class="shop-sub">{{ $tenantSlug }}.{{ config('tenancy.base_domain') }}</div> -->
             </div>
         </a>
         <div class="nav-links">
